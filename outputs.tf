834a110710a410fba52a349e37a3c1f798eb9a38
Daniel Randall <dgrandall@me.com>
2020-02-19 16:47:06 -0700

    get the ip or hostname, depending on which is set

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "lb_hostname" {
-  value       = kubernetes_service.lb.load_balancer_ingress
+output "lb_address" {
+  value       = length(kubernetes_service.lb.load_balancer_ingress[0].ip) > 0 ? kubernetes_service.lb.load_balancer_ingress[0].ip : kubernetes_service.lb.load_balancer_ingress[0].hostname
   description = "The hostname of the LB created by kubernetes"
 }
