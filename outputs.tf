7ea3121d567b52beb67420c1dab722de50bc0c74
Daniel Randall <dgrandall@me.com>
2020-02-19 16:37:43 -0700

    use load balancer ingress

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "lb_hostname" {
-  value       = kubernetes_service.lb.hostname
+  value       = kubernetes_service.lb.load_balancer_ingress
   description = "The hostname of the LB created by kubernetes"
 }
