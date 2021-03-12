b4884487ec20df468492848aac3bc2396a91b89c
Brayden Winterton <bwinterton@gmail.com>
2021-03-12 08:55:58 -0700

    Fix lb hostname output for Kubernetes provider 2.0 (#15)

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "lb_address" {
-  value       = length(kubernetes_service.lb.load_balancer_ingress[0].ip) > 0 ? kubernetes_service.lb.load_balancer_ingress[0].ip : kubernetes_service.lb.load_balancer_ingress[0].hostname
+  value       = length(kubernetes_service.lb.status[0].load_balancer[0].ingress[0].ip) > 0 ? kubernetes_service.lb.status[0].load_balancer[0].ingress[0].ip : kubernetes_service.lb.status[0].load_balancer[0].ingress[0].hostname
   description = "The hostname of the LB created by kubernetes"
 }
 
